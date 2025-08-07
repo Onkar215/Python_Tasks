a44365f7b130648e1ba405104fdc5cd885c7b287
Onkar215 <onkar.dhillon@my.rongotai.school.nz>
2025-08-08 10:41:19 +1200

    Update Number_22.py

--- Task 1/Number_22.py
+++ Task 1/Number_22.py
@@ -1,1 +1,6 @@
-
+StartingBalance = 100
+ShortTrack = 1.79
+MediumTrack = 2.5
+LongTrack = 4.5
+AfterBalance = StartingBalance - (ShortTrack * 3) - (MediumTrack * 4) - (LongTrack * 2)
+print("AfterBalance", AfterBalance)

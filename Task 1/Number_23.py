c0be4d2a6827b099b7dab9355e51b7fb24f48512
Onkar215 <onkar.dhillon@my.rongotai.school.nz>
2025-08-08 10:42:16 +1200

    Update Number_23.py

--- Task 1/Number_23.py
+++ Task 1/Number_23.py
@@ -1,1 +1,11 @@
-
+PunnetWeight = .25
+BoxWeight = 1.5
+BucketWeight = 7
+BerryWeight = 86
+TotalPunnetWeight = 14 * PunnetWeight
+TotalBoxWeight = 13 * BoxWeight
+TotalBucketWeight = 9 * BucketWeight
+TotalWeight = TotalPunnetWeight + TotalBoxWeight + TotalBucketWeight
+print("TotalWeight", TotalWeight)
+chargeofBerryWeight= TotalWeight * 5
+print("ChargeOfBerryweight =","$", chargeofBerryWeight)

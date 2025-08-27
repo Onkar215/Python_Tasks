f5cfc999cd0563dc5f7357ad33e1b8c6eb36eac7
Onkar215 <onkar.dhillon@my.rongotai.school.nz>
2025-08-28 10:07:46 +1200

    Update Number_22.py

--- Task 1/Number_22.py
+++ Task 1/Number_22.py
@@ -1,6 +1,10 @@
 StartingBalance = 100
 ShortTrack = 1.79
 MediumTrack = 2.5
-LongTrack = 4.5
-AfterBalance = StartingBalance - (ShortTrack * 3) - (MediumTrack * 4) - (LongTrack * 2)
-print("AfterBalance", AfterBalance)
+longTrack = 4.5
+AfterShortTrack = (ShortTrack* 3) 
+AfterMediumTrack = (MediumTrack * 4)
+AfterLongTrack = (longTrack * 2)
+CostAltogether = AfterLongTrack + AfterMediumTrack + AfterShortTrack
+FinalBalance = StartingBalance - CostAltogether
+print("The New Balance is", FinalBalance)

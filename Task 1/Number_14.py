55a7604fa2d06c9ee6816d285556ddded88033a3
Onkar215 <onkar.dhillon@my.rongotai.school.nz>
2025-08-28 10:05:23 +1200

    Update Number_14.py

--- Task 1/Number_14.py
+++ Task 1/Number_14.py
@@ -1,6 +1,6 @@
 NZD = 250.00
 AUS = NZD / 1.08
 USD = NZD / 1.40
-print("NZD", NZD)
-print("AUS", AUS)
-print("USD", USD)
+print("The money in NZD is", NZD)
+print("The money in AUS is", AUS)
+print("The money in USD is", USD)

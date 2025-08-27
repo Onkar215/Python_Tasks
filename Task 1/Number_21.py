5224fc5008c3890e472220b63bc443ba8b03640a
Onkar215 <onkar.dhillon@my.rongotai.school.nz>
2025-08-28 10:07:28 +1200

    Update Number_21.py

--- Task 1/Number_21.py
+++ Task 1/Number_21.py
@@ -3,10 +3,10 @@ Orginal95Price = 2.17
 Orginal91Price = 2.06
 AfterDieselvalue = OrginalDieselPrice * 0.10
 AfterDieselPrice = OrginalDieselPrice + AfterDieselvalue
-print("AfterDieselPrice", AfterDieselPrice)
+print("The after Diesel price is", AfterDieselPrice)
 After95Vvalue = Orginal95Price * 0.10
 After95Price = Orginal95Price + After95Vvalue
-print("After95Price", After95Price)
+print("The after 95 price is", After95Price)
 After91value = Orginal91Price * 0.10
 After91Price = Orginal91Price + After95Vvalue
-print("After91Price", After91Price)
+print("The after 91 price is", After91Price)
